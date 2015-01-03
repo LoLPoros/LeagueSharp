d501963c59e8f67bcd04d4f33d6e81d3d5cf3815
LoLPoros <lolporos@hotmail.com>
2015-01-03 17:54:01 +0800

    First Release!

---
+++ Hi Im Gosu Greetings/Hi Im Gosu Greetings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace Hi_Im_Gosu_Greetings
+
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Game.Say("/all Hi I'm Gosu ! GLHF ");
+        }
+    }
+}
